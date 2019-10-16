c832b5efc677642fa45a73a7ff7ec197f6871cbe
sahil <sahil11panindre@gmail.com>
2019-10-16 15:17:45 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,11 +1,11 @@
 import mod1
 config = {
-    "apiKey": "AIzaSyCoU34ezqb7B5JLOhMhfj0IU5JFIoAwBos",
-    "authDomain": "frutcon-2cf1d.firebaseapp.com",
-    "databaseURL": "https://frutcon-2cf1d.firebaseio.com",
-    "projectId": "frutcon-2cf1d",
-    "storageBucket": "frutcon-2cf1d.appspot.com",
-    "messagingSenderId": "774855078271"
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": ""
 }
 
 firebase_upload = mod1.lets_do_it(config)
